3ecd69404ce370dfbaa8f506d580372a5ca5b4ec
makyia bonner <makyiabonner@gmail.com>
2025-03-26 17:41:15 -0400

    add html boilerplate and js pseudocode

---
+++ game.js
@@ -0,0 +1,18 @@
+/*
+need array consisting of choices
+
+need to generate computer choices
+    -random number generator to pick from choices array length
+    -return choice
+
+need to generate human choices
+    -prompt user to input choice
+    -if user choice isn't valid, alert user choice isn't optional
+    -return choice in lowercase format to match array's options
+    
+need to generate winner/loser
+    -generate game flow as far as rules(paper beats rock, rock beats scissors, etc.) 
+      to determine winner and loser
+    -if user and cpu have the same choice, return draw
+    -use alert to show user the round winner/loser along with current game and record
+*/
